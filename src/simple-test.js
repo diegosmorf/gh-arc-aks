11a2fa4ab01cde61d7f7c841f2e94783f16134ef
Diego Cardoso <DiegoCardoso@microsoft.com>
2024-09-18 14:08:49 -0300

    feat: add k6 tests

--- src/simple-test.js
+++ src/simple-test.js
@@ -1,12 +1,30 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from "k6";
+import http from "k6/http";
+import { Options } from "k6/options";
 
-export const options = {
-  vus: 1,
-  duration: '5s',
+export let options: Options = {
+  vus: 50, // Define the number of virtual users
+  duration: "15m", // Define how long the test will run
+  tags: {
+    testid: `${getCurrentUTCTimestamp()}-runners`,
+  },
+};
+
+export default () => {
+  dispatchGitHubWorkflow();
+};
+
+export function getCurrentUTCTimestamp() {
+  const now = new Date();
+  return now.toISOString().replace("T", " ").substring(0, 19);
 }
 
-export default function () {
-  http.get('http://google.com');
+export function dispatchGitHubWorkflow() {
+  const url = "https://test-api.k6.io";
+
+  let res = http.get(url);
+  check(res, {
+    "is status 200": (r) => r.status === 200,
+  });
   sleep(1);
 }
